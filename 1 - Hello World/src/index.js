456bee77bca504389da07baf9c015ae95cf64b24
SunilSyal <sunil.syal@gmail.com>
2017-02-24 08:56:29 +0000

    First Draft
    First Draft of Hello World App

---
+++ 1 - Hello World/src/index.js
@@ -0,0 +1,22 @@
+var Alexa = require('alexa-sdk');
+
+exports.handler = function(event, context, callback) {
+    var alexa = Alexa.handler(event, context);
+
+    alexa.registerHandlers(handlers);
+    alexa.execute();
+};
+
+var handlers = {
+    'LaunchRequest': function() {
+        this.emit('LaunchIntent');
+    },
+
+    'LaunchIntent': function() {
+        this.emit(':ask', "Hello how are you?");
+    },
+
+    'HelloIntent': function() {
+        this.emit(':tell', "Good to hear from you. You have successfully tested hello world skill. Good bye.");
+    }
+};
